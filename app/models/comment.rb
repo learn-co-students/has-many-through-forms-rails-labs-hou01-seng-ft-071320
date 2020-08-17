b48f890b31ed06e28275b4a0bfb9ffc968050c2b
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-16 23:30:47 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
-end
+  
+  def user_attributes=(user_attribute)
+    self.user=User.find_or_create_by(user_attribute)
+    end
+  end
