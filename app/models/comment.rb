621337298683e2f38a6ec0d0f99f361c7e7061ad
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-17 22:59:41 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,9 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user
   
   def user_attributes=(user_attribute)
-    self.user=User.find_or_create_by(user_attribute) 
+    if user_attribute[:username].present?
+    self.user=User.create(username: user_attribute[:username])
     end
   end
+
+end
